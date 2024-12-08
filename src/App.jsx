e29fee4e5fe797071495a3efd13e857c14fa0972
p@ren <ntimprosper308@gmail.com>
2024-12-08 07:03:15 +0000

    Reorder the buttons using divs

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,34 @@ function App() {
         <div className="mother">
           <div className="screen">0</div>
           <div className="keypad">
-            <button>AC</button>
-            <button>DEL</button>
-            <button>+</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>*</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
+            <div className="row1">
+              <button className='AC'>AC</button>
+              <button className='DEL'>DEL</button>
+              <button className='A'>+</button>
+            </div>
+            <div className="row2">
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>*</button>
+            </div>
+            <div className="row3">
+              <button>4</button>
+              <button>5</button>
+              <button>6</button>
+              <button>+</button>
+            </div>
+            <div className="row4">
             <button>7</button>
             <button>8</button>
             <button>9</button>
             <button>-</button>
+            </div>
+            <div className="row5">
             <button>0</button>
             <button>=</button>
             <button>/</button>
+            </div>
           </div>
         </div>
       </div>
